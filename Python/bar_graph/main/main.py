162ab588e475dec4ab8113e34a0d8359873f84c1
maxpaulino <max.paulinor@gmail.com>
2023-03-02 17:15:59 -0600

    Cleaned up segregation CSV
    Also inserted all of the populations for each city

--- Python/bar_graph/main/main.py
+++ Python/bar_graph/main/main.py
@@ -1,20 +1,14 @@
-import matplotlib as plt
 import pandas as pd
+import matplotlib as plt
+import seaborn as sns
 
-
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    fatal = pd.read_csv('resources/fatal_encounters.csv', delimiter=',')
+    segregation = pd.read_csv('resources/seg_per_city.csv', delimiter=',')
+
+    print(fatal.head())
+    print(segregation.head())
+
+
+
