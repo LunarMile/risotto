44c51b12374c8fc53f43662175e9473631ccaacb
maxpaulino <max.paulinor@gmail.com>
2023-03-04 16:44:48 -0600

    Video Upload

--- Python/bar_graph/main/renamer.py
+++ Python/bar_graph/main/renamer.py
@@ -1,12 +1,12 @@
 import os
+from datetime import datetime
 
-path = "/Users/maxpaulino/Desktop/Java/LunarMile/fusilli/Python/bar_graph/numbered copy/"
-files = os.listdir(path)
-
+path = "/Users/maxpaulino/Desktop/Java/LunarMile/fusilli/Python/bar_graph/finalphoto_copy copy/"
+files = list(os.listdir(path))
+files.sort()
 for i in range(len(files)):
     if files[i] != '.DS_Store':
-        splitOnDot = files[i].split(".")
-        splitted = splitOnDot[0].split("_")
         os.replace(path+files[i], str(i) + ".png")
 
 print(f"After Renaming: {os.listdir(path)}")
+
